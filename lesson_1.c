bc57ea8085766df32d927093e920424631902aa1
abdullah kepenek <112279959+F4tihh@users.noreply.github.com>
2025-10-01 12:56:43 +0300

    start

---
+++ lesson_1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    int sayi = 10;
+
+    // Eğer sayı 5'ten büyükse bu koşul çalışır
+    if (sayi > 5) {
+        printf("Sayi 5'ten buyuktur.\n");
+    }
+
+    return 0;
+}
